691b9bf1b79e15eb399009ec8cf308395dfd654f
JesusUribe3 <chucho.jo33@gmail.com>
2022-04-03 17:53:51 -0500

    Se agrega el proyecto

---
+++ Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿
+namespace PROYECTO_MERCADO
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnllogin = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lbllogin = new System.Windows.Forms.Label();
+            this.lblusuario = new System.Windows.Forms.Label();
+            this.lblcontraseña = new System.Windows.Forms.Label();
+            this.txtcontraseña = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pnllogin.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnllogin
+            // 
+            this.pnllogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(120)))), ((int)(((byte)(200)))));
+            this.pnllogin.Controls.Add(this.pictureBox1);
+            this.pnllogin.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnllogin.Location = new System.Drawing.Point(0, 0);
+            this.pnllogin.Name = "pnllogin";
+            this.pnllogin.Size = new System.Drawing.Size(266, 450);
+            this.pnllogin.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Image = global::PROYECTO_MERCADO.Properties.Resources.usuario;
+            this.pictureBox1.Location = new System.Drawing.Point(51, 142);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(147, 128);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lbllogin
+            // 
+            this.lbllogin.AutoSize = true;
+            this.lbllogin.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbllogin.ForeColor = System.Drawing.Color.DimGray;
+            this.lbllogin.Location = new System.Drawing.Point(457, 56);
+            this.lbllogin.Name = "lbllogin";
+            this.lbllogin.Size = new System.Drawing.Size(147, 31);
+            this.lbllogin.TabIndex = 1;
+            this.lbllogin.Text = "INGRESAR";
+            // 
+            // lblusuario
+            // 
+            this.lblusuario.AutoSize = true;
+            this.lblusuario.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblusuario.ForeColor = System.Drawing.Color.LightGray;
+            this.lblusuario.Location = new System.Drawing.Point(310, 164);
+            this.lblusuario.Name = "lblusuario";
+            this.lblusuario.Size = new System.Drawing.Size(126, 27);
+            this.lblusuario.TabIndex = 2;
+            this.lblusuario.Text = "USUARIO :";
+            // 
+            // lblcontraseña
+            // 
+            this.lblcontraseña.AutoSize = true;
+            this.lblcontraseña.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblcontraseña.ForeColor = System.Drawing.Color.LightGray;
+            this.lblcontraseña.Location = new System.Drawing.Point(310, 231);
+            this.lblcontraseña.Name = "lblcontraseña";
+            this.lblcontraseña.Size = new System.Drawing.Size(181, 27);
+            this.lblcontraseña.TabIndex = 3;
+            this.lblcontraseña.Text = "CONTRASEÑA :";
+            // 
+            // txtcontraseña
+            // 
+            this.txtcontraseña.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.txtcontraseña.Location = new System.Drawing.Point(563, 164);
+            this.txtcontraseña.Name = "txtcontraseña";
+            this.txtcontraseña.Size = new System.Drawing.Size(184, 20);
+            this.txtcontraseña.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.textBox1.Location = new System.Drawing.Point(563, 238);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(184, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtcontraseña);
+            this.Controls.Add(this.lblcontraseña);
+            this.Controls.Add(this.lblusuario);
+            this.Controls.Add(this.lbllogin);
+            this.Controls.Add(this.pnllogin);
+            this.Name = "frmLogin";
+            this.Opacity = 0.85D;
+            this.Text = "Login";
+            this.pnllogin.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnllogin;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lbllogin;
+        private System.Windows.Forms.Label lblusuario;
+        private System.Windows.Forms.Label lblcontraseña;
+        private System.Windows.Forms.TextBox txtcontraseña;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
